8f90793ee4d0f5b11fb5468b8c89018b401683c2
vladdd955 <74567141+vladdd955@users.noreply.github.com>
2022-11-14 23:50:41 +0200

    Update index5.php

--- php-Task/Majas darbs 2.2 arrays/index5.php
+++ php-Task/Majas darbs 2.2 arrays/index5.php
@@ -1,1 +1,90 @@
 <?php
+
+
+$board = [
+  [" "," "," "],
+  [" "," "," "],
+  [" "," "," "],
+];
+
+
+function displayBoard($board)
+{
+    echo " {$board[0][0]}  | {$board[0][1]}  | {$board[0][2]}  \n";
+    echo "----+----+----\n";
+    echo " {$board[1][0]}  | {$board[1][1]}  | {$board[1][2]}  \n";
+    echo "----+----+----\n";
+    echo " {$board[2][0]}  | {$board[2][1]}  | {$board[2][2]}  \n";
+}
+
+
+displayBoard($board);
+
+$playerX = "X";
+$playerO = "O";
+$count = 0;
+$currentPlayer = "X";
+
+while(true) {
+    $count++;
+
+    $userSelection = readline("Enter chose [row,column] separate by , -> ");
+    [$x, $y] = explode(",", $userSelection);
+
+    if ($x === null && $y === null) {
+        echo "Invalid input ";
+        echo  "\n";
+        continue;
+
+    }
+    if ($board[$x][$y] === " ") {
+        $board[$x][$y] = $currentPlayer;
+        displayBoard($board);
+        echo  "\n";
+    } else {
+        echo "Invalid input, try again, make sure that board cell is empty :) \n";
+        continue;
+    }
+
+
+
+
+            // Rows
+    if ($board[0][0] === $board[0][1] && $board[0][0] === $board[0][2] && $board[0][2]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Rows combination \n";
+        break;
+    } elseif ($board[1][0] === $board[1][1] && $board[1][0] === $board[1][2] && $board[1][2]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Rows combination \n";
+        break;
+    } elseif ($board[2][0] === $board[2][1] && $board[2][0] === $board[2][2] && $board[2][2]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Rows combination \n";
+        break;
+                // Column
+    } elseif ($board[0][0] === $board[1][0] && $board[0][0] === $board[2][0] && $board[2][0]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Column combination \n";
+        break;
+    } elseif ($board[0][1] === $board[1][1] && $board[0][1] === $board[2][1] && $board[2][1]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Column combination \n";
+        break;
+    } elseif ($board[0][2] === $board[1][2] && $board[0][2] === $board[2][2] && $board[2][2]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Column combination \n";
+        break;
+                // Diagonal
+    } elseif ($board[0][0] === $board[1][1] && $board[0][0] === $board[2][2] && $board[2][2]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Diagonal combination \n";
+        break;
+    } elseif ($board[2][0] === $board[1][1] && $board[2][0] === $board[0][2] && $board[0][2]  != " ") {
+        echo $currentPlayer . "\n" . "Congratulation you Win in Diagonal combination \n";
+        break;
+    }
+
+
+    if ($count === 10){
+        echo "Game is a TIE!!!!";
+        break;
+    }
+
+    $currentPlayer = $currentPlayer === $playerX ? $playerO : $playerX;
+
+
+}
